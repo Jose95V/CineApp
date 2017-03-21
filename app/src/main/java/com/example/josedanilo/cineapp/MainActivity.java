395ec748507aa3dce1b02fac7e2d432a57c989e1
Jose Vallecillo <jose.vallecillo@ujcv.edu.hn>
2017-03-20 21:01:27 -0600

    iniciando programacion en MainActivity

--- app/src/main/java/com/example/josedanilo/cineapp/MainActivity.java
+++ app/src/main/java/com/example/josedanilo/cineapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.josedanilo.cineapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,8 +14,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    public static Context mContext;
+    JSONObject CineJason=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,13 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setTitle("Peliculas");
+
+
+        mContext=this;
+        String url="http://bdcinemania.esy.es/obtener_peliculas.php";
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +52,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
     }
 
     @Override
@@ -94,4 +110,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
